6ca973cdf58d8e27af535c0e59a7f49ac4ec20e7
Miki Watanabe <watami03@gmail.com>
2025-10-14 18:51:42 +0200

    add background image

--- frontend/components/assistant-ui/thread.tsx
+++ frontend/components/assistant-ui/thread.tsx
@@ -40,6 +40,10 @@ export const Thread: FC = () => {
           className="aui-root aui-thread-root @container flex h-full flex-col bg-background"
           style={{
             ["--thread-max-width" as string]: "44rem",
+            backgroundImage: 'url("/dog_akitainu.png")',
+            backgroundPosition: "center",
+            backgroundSize: "auto",
+            backgroundRepeat: "no-repeat"
           }}
         >
           <ThreadPrimitive.Viewport className="aui-thread-viewport relative flex flex-1 flex-col overflow-x-auto overflow-y-scroll px-4">
