cb0da4e0775bb1473d5f70faaa44246101265b2c
Miki Watanabe <watami03@gmail.com>
2025-10-23 22:10:21 +0200

    refactor: extract content after </think>

--- agent/cli-agent.py
+++ agent/cli-agent.py
@@ -1,6 +1,4 @@
 import asyncio
-import sys
-from langgraph_sdk import get_client
 from agent import make_graph
 
 async def main():
@@ -10,12 +8,9 @@ async def main():
 
     while True:
         user_input = input("You: ").strip()
-        print("**** user_input **** ", user_input)
         if user_input.lower() in {"exit", "quit"}:
             print("Bye!")
             break
-
-        print("agent:", end=" ", flush=True)
 
         inputs = {"messages": [{"role": "user", "content": user_input}]}
 
@@ -29,9 +24,17 @@ async def main():
                 last = msgs[-1]
 
                 if getattr(last, "type", None) in ("ai", "assistant"):
-                    // TODO rewrite
                     text = last.content
-                    print("text ", text)
+                    if "</think>" in text:
+                        answer_text = text.split("</think>", 1)[1].strip()
+                        if "</error>" in answer_text:
+                            answer_text = text.split("</error>", 1)[1].strip()
+                        elif  "</answer>" in answer_text:
+                            answer_text = text.split("</answer>", 1)[1].strip()
+                        elif  "</response>" in answer_text:
+                            answer_text = text.split("</response>", 1)[1].strip()
+
+                    print(answer_text)
                     
 if __name__ == "__main__":
     asyncio.run(main())
